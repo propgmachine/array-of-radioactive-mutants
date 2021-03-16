5c45fe82de7da50574d565b74de3d0e43126c82e
propgmachine <77746698+propgmachine@users.noreply.github.com>
2021-03-15 20:25:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,40 @@
  * YOUR CODE HERE *
  ******************/
 
+function addMeToEnd(array) {
+  array.push('Colin');
+}
 
+function addMeToStart(array) {
+  array.unshift('Colin')
+}
 
+function changeLast(array, value) {
+  array[array.length - 1] = value;
+}
 
+function changeAllValuesTo(array, value) {
+  for 
+}
 
+function oddOrEven(array) {
+  for (let i = 0; i < array.length; i++) {
+
+    if (array[1] % 2 === 1) {
+      array[i] = 'odd';
+    }
+  
+    else {
+      array[i] = 'even';
+    }
+  }
+
+  function changeNextThreeToValue(index, array, value) {
+    for (let i = index; i < index + 3; i++) {
+      array[i] = value;
+    }
+  }
+}
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
  *********************************/
